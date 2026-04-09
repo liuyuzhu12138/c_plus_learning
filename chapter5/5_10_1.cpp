3687b046ab3f86f2d6ebce629f9b5d68dd33fd58
liuyuzhu12138 <1292578260@qq.com>
2026-04-09 15:42:57 +0800

    添加示例代码：演示相等与赋值的区别。与5_10的区别在于：5_10_1在for loop的测试语句中增加了索引是否超出数组quizscores最后一位成员的测试，以此来避免forl loop无法停止，这是一个好习惯。

---
+++ chapter5/5_10_1.cpp
@@ -0,0 +1,17 @@
+//equal.cpp -- equality vs assignment
+#include <iostream>
+#include <vector>
+int main()
+{
+    using namespace std;
+    vector<int> quizscores { 20, 20, 20, 20, 20, 19, 20, 18, 20, 20 };
+    cout << "Doing it right:" << endl;
+    for (int i = 0; i < quizscores.size() && quizscores[i] == 20; i++) //在循环中测试数组的值和索引的值，以避免超出数组的最后一位成员
+        cout << "quiz" << i << " is a 20" << endl;
+//Warning: you may prefer reading about this program
+//to actually running it.
+ cout << "Doing it dangerously wrong:" << endl;
+ //for (int i = 0; quizscores[i] = 20; i++)
+ //    cout << "quiz" << i << " is a 20" << endl;
+    return 0;
+}
