6e23ba0838afc7ea9de76b67633c333ef13b5c06
liuyuzhu12138 <1292578260@qq.com>
2026-04-10 20:05:59 +0800

    添加示例代码：使用clock()函数实现时间延迟循环

---
+++ chapter5/5_14.cpp
@@ -0,0 +1,17 @@
+//waiting.cpp -- using clock() in a time-delay loop
+#include <iostream>
+#include <ctime> //describes clock() function, clock_t type
+int main()
+{
+    using namespace std;
+    cout << "Enter the delay time, in seconds: ";
+    float secs;
+    cin >> secs;
+    clock_t delay = secs * CLOCKS_PER_SEC; //convert to clock ticks
+    cout << "Starting\a\n";
+    clock_t start = clock();
+    while (clock() - start < delay)
+        ; //do nothing
+    cout << "Done\a\n";
+    return 0;
+}
